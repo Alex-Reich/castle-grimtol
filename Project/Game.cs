f69371e86ac8493f47c428dae70158abea99c217
Alex Reich <atrain.reich@gmail.com>
2018-06-20 16:00:01 -0600

    Added sleep function. Added victory condition - use key in car.

--- Project/Game.cs
+++ Project/Game.cs
@@ -17,10 +17,7 @@ namespace CastleGrimtol.Project
             bool playing = true;
             while (playing)
             {
-                // Console.WriteLine("You win... until the DLC comes out and you find out it was all a dream!");
-                // System.Console.WriteLine("Start over? y/n");
-                // if (Console.Read() == 'n') { playing = false; };
-                System.Console.Write("What would you like to do?");
+                System.Console.Write("What would you like to do? ");
                 string userInput = Console.ReadLine();
                 string[] input = userInput.ToLower().Split(" ");
 
@@ -34,7 +31,7 @@ namespace CastleGrimtol.Project
                             {
                                 CurrentRoom = CurrentRoom.Go(input[1]);
                                 Console.Clear();
-                                System.Console.WriteLine($"You are now in the {CurrentRoom.Name}");
+                                System.Console.WriteLine($"You are now in the {CurrentRoom.Name}. {CurrentRoom.Description}");
                             }
                             else
                             {
@@ -50,7 +47,7 @@ namespace CastleGrimtol.Project
                             {
                                 CurrentRoom = CurrentRoom.Go(input[1]);
                                 Console.Clear();
-                                System.Console.WriteLine($"You are now in the {CurrentRoom.Name}");
+                                System.Console.WriteLine($"You are now in the {CurrentRoom.Name}. {CurrentRoom.Description}");
                             }
                             else
                             {
@@ -66,7 +63,7 @@ namespace CastleGrimtol.Project
                             {
                                 CurrentRoom = CurrentRoom.Go(input[1]);
                                 Console.Clear();
-                                System.Console.WriteLine($"You are now in the {CurrentRoom.Name}");
+                                System.Console.WriteLine($"You are now in the {CurrentRoom.Name}. {CurrentRoom.Description}");
                             }
                             else
                             {
@@ -82,7 +79,7 @@ namespace CastleGrimtol.Project
                             {
                                 CurrentRoom = CurrentRoom.Go(input[1]);
                                 Console.Clear();
-                                System.Console.WriteLine($"You are now in the {CurrentRoom.Name}");
+                                System.Console.WriteLine($"You are now in the {CurrentRoom.Name}. {CurrentRoom.Description}");
                             }
                             else
                             {
@@ -111,6 +108,7 @@ namespace CastleGrimtol.Project
                         else
                         {
                             Console.Clear();
+                            System.Console.WriteLine($"You are in the {CurrentRoom.Name}: {CurrentRoom.Description}.");
                             System.Console.WriteLine("There are no items currently in this room.");
                         }
                         break;
@@ -122,6 +120,7 @@ Look - Examines your current surroundings
 Go <Direction> - Moves the player from room to room
 Use <ItemName> - Uses an item in a room or from your inventory
 Take <ItemName> - Places an item into the player inventory and removes it from the room
+i - Lists items in your inventory
 Quit - Quits the Game");
                         break;
                     case "take":
@@ -130,15 +129,41 @@ Quit - Quits the Game");
                         break;
                     case "use":
                         Console.Clear();
-                        UseItem(input[1]);
+                        if (input[1] != null)
+                        {
+                            UseItem(input[1]);
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("You must enter an item name alongside the 'use' command");
+                        }
+                        break;
+                    case "i":
+                        if (CurrentPlayer.Inventory.Count > 0)
+                        {
+                            Console.WriteLine("You are currently holding:");
+                            foreach (var i in CurrentPlayer.Inventory)
+                            {
+                                Console.WriteLine(i.Name);
+                            }
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("Your inventory is empty! Take something!");
+                        }
                         break;
                     case "quit":
                         playing = false;
                         Console.Clear();
                         System.Console.WriteLine("Thanks for playing!");
                         break;
+                    case "sleep":
+                        playing = false;
+                        Console.Clear();
+                        System.Console.WriteLine("You decide to go back to sleep, dismissing the troubles and stresses of the day. The consequences of your actions will certainly be a problem for 'future you,' but for now, you choose to return to seize the day and return to sleep. YOU ARE A WINNER!");
+                        break;
                     default:
-                        System.Console.WriteLine("Please enter a proper action. Type 'help' if you are stuck.");
+                        System.Console.WriteLine("Please enter a proper action. Type 'help' if you are stuck");
                         break;
                 }
 
@@ -159,52 +184,48 @@ With a jolt, you awake. You open your eyes and find yourself knotted in blankets
             
 You hear a buzzing sound coming from your phone located on the floor across the room, next to a pile of your clothes and the garage door opener. You had better check your phone!
 
-Goal: Get dressed and do all other 'prepare for the day' tasks, get in your car, and drive away
+Goal: Get dressed and do all the other 'prepare for the day' tasks, get in your car, and drive away
+For a list of possible actions type 'help'
             ");
         }
         public void TakeItem(string itemName)
         {
-            foreach (var i in CurrentRoom.Items.ToArray())
+            var item = CurrentRoom.Items.Find(i => i.Name.ToLower().Contains(itemName));
+
+            if (CurrentRoom.Items.Contains(item))
             {
-                var name = i.Name.ToLower();
-                if (name.Contains(itemName))
-                {
-                    CurrentPlayer.Inventory.Add(i);
-                    CurrentRoom.Items.Remove(i);
-                    System.Console.WriteLine($"You picked up {i.Name}");
-                    break;
-                }
-                else
-                {
-                    System.Console.WriteLine("There is nothing to pick up by that name.");
-                }
+                CurrentPlayer.Inventory.Add(item);
+                CurrentRoom.Items.Remove(item);
+                System.Console.WriteLine($"You picked up {item.Name}");
             }
+            else
+            {
+                System.Console.WriteLine("There is nothing to pick up by that name.");
+            }
+
 
         }
         public void UseItem(string itemName)
         {
 
-
             var item = CurrentPlayer.Inventory.Find(i => i.Name.ToLower().Contains(itemName));
             if (CurrentPlayer.Inventory.Contains(item))
             {
+                if (item.Name == "Car Keys" && CurrentRoom.Name == "Car")
+                {
+                    Console.WriteLine("You insert the keys into the ignition and fire up the engine. You throw it into reverse and gun it, leaving tire marks in your garage. You're on your way; mission accomplished!");
+                    System.Console.WriteLine("YOU WIN!!");
+                    System.Console.WriteLine("But did you complete all the daily tasks before you left?? Play again to try and finish everything before leaving the house.");
+                    System.Console.WriteLine("Start over? y/n");
+                    // if (Console.Read() == 'n') {var playing = false; };
+                    return;
+                }
                 System.Console.WriteLine($"{item.Description}");
             }
             else
             {
                 System.Console.WriteLine("You don't have the item you are trying to use.");
             }
-
-
-
-            // var item = CurrentPlayer.Inventory.Find(i => i.Name.Contains(itemName));
-
-            // if (CurrentPlayer.Inventory.Contains(item))
-            // {
-            // }
-            // if (!CurrentPlayer.Inventory.Contains(item))
-            // {
-            // }
 
         }
         public void CreateRooms()
@@ -215,7 +236,7 @@ Goal: Get dressed and do all other 'prepare for the day' tasks, get in your car,
             var garage = new Room("Garage", "Cold, damp. Boxes of assorted junk line the walls.  Exits: e -> kitchen, w -> car");
             var car = new Room("Car", "It still runs... Sometimes.   Exits: e -> garage");
 
-            bedroom.Items.Add(new Item("Clothes", "You put on your clothes... for the fourth day in a row."));
+            bedroom.Items.Add(new Item("Clothes", "You put on your favorite shirt and shorts... for the fifth day in a row."));
             bedroom.Items.Add(new Item("Garage Door Opener", "A garage door opens somewhere."));
             bedroom.Items.Add(new Item("Cell Phone", "5 missed calls, 6 unread messages. All mention you needing to be somewhere fast!"));
             bedroom.exits.Add("e", bathroom);
@@ -237,7 +258,7 @@ Goal: Get dressed and do all other 'prepare for the day' tasks, get in your car,
             garage.exits.Add("w", car);
             _rooms.Add(garage);
 
-            car.Items.Add(new Item("Taco Bell Doritos Locos Taco", "You eat the taco and then realize, 'I haven't ordered taco bell for over a month!"));
+            car.Items.Add(new Item("Taco Bell Doritos Locos Taco", "You eat the taco and then realize that you haven't ordered taco bell for over a month!"));
             car.exits.Add("e", garage);
             _rooms.Add(car);
 
